aeabc566b8bd6a5e8c71da6e57f5eef506ea948d
Rustam Issabekov <rus.issabekov@gmail.com>
2018-12-21 20:01:37 +1100

    Added react and webpack base provided by create-react-app utility
    Added react and webpack base provided by create-react-app utility

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Here be burger builder!
+      </div>
+    );
+  }
+}
+
+export default App;
