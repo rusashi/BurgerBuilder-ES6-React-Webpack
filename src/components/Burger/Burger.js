7f72dc5d21ea07386320b31751753f228c7ca90d
Rustam Issabekov <rus.issabekov@gmail.com>
2018-12-22 12:07:45 +1100

    Added dummy BuildControls element

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+    let ingredients = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])].map((_,i) => {
+                return <BurgerIngredient key={ingredient+1} type={ingredient}/>
+            }); 
+        }).reduce((arr,el) => {return arr.concat(el)},[]);
+    
+    if (ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients!</p>;
+    }
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
